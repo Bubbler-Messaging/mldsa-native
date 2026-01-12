df60892295fc37660e2686acf81fd6bbdcabe3d8
github-action-benchmark <github@users.noreply.github.com>
2026-01-12 17:04:37 +0000

    add SpacemiT K1 8 (Banana Pi F3) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for cdb6aa21d757ec1f487f1510f3e8b385e99d9657

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768236974810,
+  "lastUpdate": 1768237475007,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -83893,6 +83893,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 2406456,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "willie.zhao@chelpis.com",
+            "name": "willieyz",
+            "username": "willieyz"
+          },
+          "committer": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "distinct": true,
+          "id": "cdb6aa21d757ec1f487f1510f3e8b385e99d9657",
+          "message": "Port: use consistent architecture identifier\n\nThis commit ports the change from\nhttps://github.com/pq-code-package/mlkem-native/pull/1456\nto mldsa-native.\n\nCurrently, mldsa-native already uses x86_64 as the architecture\nidentifier for x86 in its existing HOL-Light proofs, and no changes are\nrequired in that regard. This commit simply aligns the surrounding\nscripts and infrastructure to consistently use x86_64 as the\narchitecture name.\n\n- Adds the related scripts to support dual architectures by introducing\n  an explicit arch parameter in the autogen process.\n\n- Replaces the arm_or_x86 flag with a unified arch flag in test scripts.\n\nSigned-off-by: willieyz <willie.zhao@chelpis.com>",
+          "timestamp": "2026-01-12T16:46:46Z",
+          "tree_id": "1b025e867776ef22eae32c7e67dc889f0ddf6c4f",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/cdb6aa21d757ec1f487f1510f3e8b385e99d9657"
+        },
+        "date": 1768237264209,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 832393,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 3249631,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 920462,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 1412945,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 5367018,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 1480647,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 2310223,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 6658290,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 2410477,
             "unit": "cycles"
           }
         ]

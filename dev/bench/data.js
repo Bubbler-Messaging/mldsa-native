4195fa7e85effc07ee8fe3fa74ee033b139ef278
github-action-benchmark <github@users.noreply.github.com>
2026-01-12 05:24:20 +0000

    add Intel Xeon 3rd gen (c6i) (no-opt) (customSmallerIsBetter) benchmark result for 09cc8d20c8ad2f4dbce7d7553e0d3d71adb674f2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768195457481,
+  "lastUpdate": 1768195460130,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -201319,6 +201319,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 458086,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "distinct": true,
+          "id": "09cc8d20c8ad2f4dbce7d7553e0d3d71adb674f2",
+          "message": "CBMC: Clean up harnesses\n\n- Fix missing pointer in sign_verify harness (sig was declared as\n  uint8_t instead of uint8_t*)\n- Remove unused variable in keccak_absorb harness\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-01-12T05:20:40Z",
+          "tree_id": "41c7b8a166c81cf7838ce9e81eb11aa1f7768ebc",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/09cc8d20c8ad2f4dbce7d7553e0d3d71adb674f2"
+        },
+        "date": 1768195454805,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 157572,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 550015,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 169531,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 268106,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 905012,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 273575,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 448453,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1158811,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 457506,
             "unit": "cycles"
           }
         ]

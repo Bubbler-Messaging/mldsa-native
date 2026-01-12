dfb00f9d87b872ca42b4e7d31815240739c2fe43
github-action-benchmark <github@users.noreply.github.com>
2026-01-12 10:47:40 +0000

    add AMD EPYC 3rd gen (c6a) (customSmallerIsBetter) benchmark result for 971ecb7646aca5a25f732b40be033676de9c5992

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768214850445,
+  "lastUpdate": 1768214860346,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -164063,6 +164063,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 192542,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "971ecb7646aca5a25f732b40be033676de9c5992",
+          "message": "Adjust alloc failure test to use context parameter\n\nThe allocation failure test uses a global bump allocator.\n\nThis commit modifies the tests config to enable context parameters,\nand pass the test context explicitly, including the bump allocator.\n\nThis removes the static globals from the allocator test as well as\nexercising the new MLD_CONFIG_CONTEXT_PARAMETER[_TYPE] options.\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-01-12T18:44:25+08:00",
+          "tree_id": "5ca833f8ffcde715060b7902fd71de523dd1fb21",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/971ecb7646aca5a25f732b40be033676de9c5992"
+        },
+        "date": 1768214821975,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 69240,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 187916,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 69177,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 120710,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 305323,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 115478,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 207057,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 403930,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 196624,
             "unit": "cycles"
           }
         ]

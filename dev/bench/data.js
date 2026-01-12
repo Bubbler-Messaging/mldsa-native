ae8e3864bdf10ed97854226395e143f19b6162aa
github-action-benchmark <github@users.noreply.github.com>
2026-01-12 06:29:24 +0000

    add Intel Xeon 4th gen (c7i) (no-opt) (customSmallerIsBetter) benchmark result for 68490efc3389e21f172e46c3d12f328505b8f0f9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768199355378,
+  "lastUpdate": 1768199364658,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -240714,6 +240714,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 269570,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "68490efc3389e21f172e46c3d12f328505b8f0f9",
+          "message": "Verify memory usage: Re-use t1/w1 buffer\n\nThis commit is the first of a series of commits reducing the stack usage of\nverification.\nIt is hoisted out from https://github.com/pq-code-package/mldsa-native/pull/751\n\nThis commit places the t1 and w1 buffers into a union saving K KiB of memory.\nOperations using it are slightly reordered such that their lifetime does not\noverlap.\nAs CBMC struggles with unions (issue 8813), we use the same workaround\npresent in signing: Use a struct by default, and a union when\nMLD_CONFIG_REDUCE_RAM is set.\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-01-12T14:26:30+08:00",
+          "tree_id": "45eaaa2c19b02d982fab277f1bd55d7176074155",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/68490efc3389e21f172e46c3d12f328505b8f0f9"
+        },
+        "date": 1768199364247,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 94405,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 333643,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 99784,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 160622,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 545845,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 161006,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 267646,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 713960,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 271433,
             "unit": "cycles"
           }
         ]

7cf2887a67728949529ca9be430df01597424e6a
github-action-benchmark <github@users.noreply.github.com>
2026-01-13 09:43:46 +0000

    add Graviton2 (customSmallerIsBetter) benchmark result for 060fdbd6432ed177f512d91dec04d6eb27e8760e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768297415769,
+  "lastUpdate": 1768297426235,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -386290,6 +386290,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 320172,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "distinct": true,
+          "id": "060fdbd6432ed177f512d91dec04d6eb27e8760e",
+          "message": "autogen: Use `rich` for progress bar\n\nThis commit modifies `autogen` to use terminal printing and\nprogress bar updates from the `rich` package as opposed to our\nprevious ad-hoc approach.\n\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>",
+          "timestamp": "2026-01-13T09:39:03Z",
+          "tree_id": "b2faf27b3fb257cf264ff1c0ab04a15de034ade9",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/060fdbd6432ed177f512d91dec04d6eb27e8760e"
+        },
+        "date": 1768297354169,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 113734,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 356249,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 118098,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 196664,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 589302,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 194763,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 323358,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 753929,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 320237,
             "unit": "cycles"
           }
         ]

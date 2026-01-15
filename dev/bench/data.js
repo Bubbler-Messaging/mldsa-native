2c74b261e129cbaa87da761ff5fe8fb1ccadecb0
github-action-benchmark <github@users.noreply.github.com>
2026-01-15 07:58:45 +0000

    add Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt) (customSmallerIsBetter) benchmark result for 1f5c0e038846c9dba7b07425970fff4ca2014200

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768463730655,
+  "lastUpdate": 1768463922617,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -20488,6 +20488,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 626757,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "1f5c0e038846c9dba7b07425970fff4ca2014200",
+          "message": "Examples: Remove MLD_CONFIG_NO_SUPERCOP from multi-level header\n\nMLD_CONFIG_NO_SUPERCOP is already set in the configuration file; it should\nnot be set again in mldsa_native_all.h.\n\n- Ports https://github.com/pq-code-package/mlkem-native/pull/1477\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-01-15T15:45:50+08:00",
+          "tree_id": "3c3557ffdcc703efad7238138ff1ca94ef58dcfb",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/1f5c0e038846c9dba7b07425970fff4ca2014200"
+        },
+        "date": 1768463918131,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 224462,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 614586,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 225601,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 391900,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1026754,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 377723,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 675057,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1375172,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 633790,
             "unit": "cycles"
           }
         ]

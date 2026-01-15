00a16dc84e018bdc49804159ee94a4a73d5a315e
github-action-benchmark <github@users.noreply.github.com>
2026-01-15 06:59:38 +0000

    add AMD EPYC 4th gen (c7a) (customSmallerIsBetter) benchmark result for 57b69ac3da3953ad6d22934a5ef311a1f1030550

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768460367321,
+  "lastUpdate": 1768460377979,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -227813,6 +227813,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 110470,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "willie.zhao@chelpis.com",
+            "name": "willieyz",
+            "username": "willieyz"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "57b69ac3da3953ad6d22934a5ef311a1f1030550",
+          "message": "Port: Allow the public header to work in C++ mode.\n\nWithout this, includes of the header must be wrapped in extern \"C\",\nwhich then causes problems if the config header includes headers that\nexpect to be included in C or C++ mode.\n\nSigned-off-by: willieyz <willie.zhao@chelpis.com>",
+          "timestamp": "2026-01-15T14:55:23+08:00",
+          "tree_id": "29d5120c14ceaaffb2e4a18d9d84e73330353aca",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/57b69ac3da3953ad6d22934a5ef311a1f1030550"
+        },
+        "date": 1768460338835,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 43354,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 136388,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 45680,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 75629,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 221325,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 75775,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 109207,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 252569,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 111481,
             "unit": "cycles"
           }
         ]

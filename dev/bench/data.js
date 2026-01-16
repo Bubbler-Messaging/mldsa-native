e38f69897388313d1af3765c31eceb2797a6f0d5
github-action-benchmark <github@users.noreply.github.com>
2026-01-16 11:04:28 +0000

    add Intel Xeon 3rd gen (c6i) (customSmallerIsBetter) benchmark result for b61e84f0c73d4ed612ffcaea4282a9d682de3f46

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768561451839,
+  "lastUpdate": 1768561468419,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -149161,6 +149161,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 152862,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "distinct": true,
+          "id": "b61e84f0c73d4ed612ffcaea4282a9d682de3f46",
+          "message": "Examples: Fix feature detection via auto.mk\n\nThe examples are using auto.mk to detect features such as AVX2.\nHowever, auto.mk depends on variables defined in compiler.mk which\nwas not included previously disabling the entire feature detection\nand causing the native examples running without the native code.\nThis caused problems like\nhttps://github.com/pq-code-package/mldsa-native/pull/889\nto go unnoticed.\n\nThis commit inlines compiler.mk into auto.mk, which resolves the issue.\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-01-16T11:01:06Z",
+          "tree_id": "7c31bbdf71e55cb48a34817ad4d2b149e05679e4",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/b61e84f0c73d4ed612ffcaea4282a9d682de3f46"
+        },
+        "date": 1768561409030,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 56544,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 181641,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 61143,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 98680,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 298590,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 100213,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 152389,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 355250,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 154131,
             "unit": "cycles"
           }
         ]

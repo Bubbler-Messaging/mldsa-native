8af78d531e60265c7d1cff2d7ec5ecfee350a0f8
github-action-benchmark <github@users.noreply.github.com>
2026-01-18 15:49:02 +0800

    add Arm Cortex-A76 (Raspberry Pi 5) benchmarks (opt) (customSmallerIsBetter) benchmark result for 263291505296d960e98c5e83901cc42affafe0df

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768722483701,
+  "lastUpdate": 1768722539962,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -455560,6 +455560,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 320374,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "andreas.hatziiliou@savoirfairelinux.com",
+            "name": "Andreas Hatziiliou",
+            "username": "L-series"
+          },
+          "committer": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "distinct": true,
+          "id": "263291505296d960e98c5e83901cc42affafe0df",
+          "message": "tests: do not generate custom build for rng test\n\nAs a follow up to e15b34f, we modify the rng failure test so that it\ndoes not generate a new build. To achieve this we do the following:\n\n* Modify components and rules to not generate a custom build.\n* Remove the custom test_rng_fail config.\n* Updated test function names to use standard crypto_sign_* API\n  instead of mld_* aliases as the mld_* namespace is no longer available\n  as we are now using the default build with\n  MLD_DEFAULT_NAMESPACE_PREFIX. Those functions which are not exposed\n  in this way by default are wrapped with MLD_API_NAMESPACE.\n\nSigned-off-by: Andreas Hatziiliou <andreas.hatziiliou@savoirfairelinux.com>\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>",
+          "timestamp": "2026-01-18T07:46:32Z",
+          "tree_id": "3a461f78c96a7b9dda2e9c0a25b6351ab0233e73",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/263291505296d960e98c5e83901cc42affafe0df"
+        },
+        "date": 1768722446508,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 113344,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 355898,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 118018,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 196553,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 589024,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 194758,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 322736,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 753170,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 320174,
             "unit": "cycles"
           }
         ]

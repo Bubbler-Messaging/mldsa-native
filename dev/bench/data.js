42067905453ceb2de6a74cfbf9e31337971a08e7
github-action-benchmark <github@users.noreply.github.com>
2026-01-19 00:50:47 +0800

    add Arm Cortex-A76 (Raspberry Pi 5) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for b1a564d81031842704d0521216c775a9966f02a8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768755036185,
+  "lastUpdate": 1768755044517,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -129567,6 +129567,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 618013,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "willie.zhao@chelpis.com",
+            "name": "willieyz",
+            "username": "willieyz"
+          },
+          "committer": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "distinct": true,
+          "id": "b1a564d81031842704d0521216c775a9966f02a8",
+          "message": "Port: Add tests for non-standard values of MLD_POLY_UNIFORM_NBLOCKS\n\nMLD_POLY_UNIFORM_NBLOCKS is a value estimating the amount of\nrejection sampling input to sample 256 polynomial coefficients.\nThis value is 5 by default\n\nThis commit port from mlkem-native, extends the CI to test the\nnon-standard values 1, 4, and 6.\n\nSigned-off-by: willieyz <willie.zhao@chelpis.com>",
+          "timestamp": "2026-01-18T16:47:33Z",
+          "tree_id": "3c9558b088fdf163068c853db35f77c658124447",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/b1a564d81031842704d0521216c775a9966f02a8"
+        },
+        "date": 1768755042087,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 212597,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 760159,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 229053,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 380558,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1252715,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 371698,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 604382,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1594019,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 618525,
             "unit": "cycles"
           }
         ]

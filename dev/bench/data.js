4e845a1eb464554e314b54765a42815216962f80
github-action-benchmark <github@users.noreply.github.com>
2026-01-20 04:32:11 +0000

    add Graviton3 (no-opt) (customSmallerIsBetter) benchmark result for f48f164cefb07f4ffa519ddda7cee670b8ee3517

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768883517756,
+  "lastUpdate": 1768883531343,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -379806,6 +379806,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 402523,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "distinct": true,
+          "id": "f48f164cefb07f4ffa519ddda7cee670b8ee3517",
+          "message": "OQS: Add missing bmi2 flag and clean up\n\nThis commit adds the missing bmi2 flag to the ML-DSA-87 yml.\nbmi2 is currently not strictly needed as asm is not using any bmi2\ninstructions, but in practice there are no CPUs that have AVX2, but not bmi2\nso we might as well keep it as we may use later (as in rejection sampling\nin mlkem-native) or the compiler may use it.\n\nThis commit also cleans up ordering of Darwin and Linux in the ymls.\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-01-20T04:27:29Z",
+          "tree_id": "77fb4bcae6ad0b7f1b1ba7e54ff8be0e11d99b06",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/f48f164cefb07f4ffa519ddda7cee670b8ee3517"
+        },
+        "date": 1768883530549,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 138511,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 484180,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 148664,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 242002,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 792768,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 241169,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 396249,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1012094,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 402525,
             "unit": "cycles"
           }
         ]

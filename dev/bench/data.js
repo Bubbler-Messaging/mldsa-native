c2059a7ea60eb0938ba46bbf1cfedc5b03d1b5a5
github-action-benchmark <github@users.noreply.github.com>
2026-01-20 08:44:16 +0000

    add Graviton3 (customSmallerIsBetter) benchmark result for df40ef1640449dc162997eafb71b04a16202f8dc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768898653086,
+  "lastUpdate": 1768898656395,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -361178,6 +361178,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 205102,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "df40ef1640449dc162997eafb71b04a16202f8dc",
+          "message": "CI: Run RNG failure test for custom configs\n\nPreviously, the RNG failure test relied on a custom configuration\nand thus had to be skipped in the custom config action.\n\nNow, the RNG failure test no longer relies on a custom config\nand can hence be included in the custom config action.\n\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>",
+          "timestamp": "2026-01-20T16:40:07+08:00",
+          "tree_id": "58601936e58542f9f8760153c532334cea6ea0c9",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/df40ef1640449dc162997eafb71b04a16202f8dc"
+        },
+        "date": 1768898585607,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 72197,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 212050,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 75733,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 127442,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 350090,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 125348,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 208143,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 448970,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 205086,
             "unit": "cycles"
           }
         ]

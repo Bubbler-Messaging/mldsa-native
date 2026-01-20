f4eb97cdcbbe51a280bdbb2cf8ee82f2c1272716
github-action-benchmark <github@users.noreply.github.com>
2026-01-20 09:37:46 +0000

    add Graviton2 (no-opt) (customSmallerIsBetter) benchmark result for a949f77252670bfc53c7ef40c0a8210eab8be730

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768901841297,
+  "lastUpdate": 1768901865638,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -425480,6 +425480,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 619149,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "willie.zhao@chelpis.com",
+            "name": "willieyz",
+            "username": "willieyz"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "a949f77252670bfc53c7ef40c0a8210eab8be730",
+          "message": "Namespace macros in KeccakP_1600_times4_SIMD256.c with the MLD_ prefix.\n\n- This commit adds the MLD_ namespace prefix to all macros in\n  KeccakP_1600_times4_SIMD256.c.\n\n- This commit also adds the mld_ prefix to the following static symbols\n  in KeccakP_1600_times4_SIMD256.c, similar with:\n  https://github.com/pq-code-package/mlkem-native/pull/1407\n\n  - keccakf1600RoundConstants\n  - rho8\n  - rho56\n\n- Without these changes, building mlkem-native together with\n  mldsa-native that defines the same symbols in a\n  single compilation unit can lead to symbol clashes.\n\nSigned-off-by: willieyz <willie.zhao@chelpis.com>",
+          "timestamp": "2026-01-20T17:32:32+08:00",
+          "tree_id": "19ad952e4d2928c836e3602bdbd6a6c17b39708a",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/a949f77252670bfc53c7ef40c0a8210eab8be730"
+        },
+        "date": 1768901864612,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 212748,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 760607,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 229139,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 380920,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1253874,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 371860,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 604510,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1595079,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 618587,
             "unit": "cycles"
           }
         ]

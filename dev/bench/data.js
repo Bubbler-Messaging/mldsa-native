905fa89190ac80f73e6339d87eb5e11458f8ec6e
github-action-benchmark <github@users.noreply.github.com>
2026-01-24 02:49:43 +0000

    add Graviton2 (no-opt) (customSmallerIsBetter) benchmark result for d7cb8422bee1572c22c3f54634da686f9ccdb177

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1769222947464,
+  "lastUpdate": 1769222983383,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -428240,6 +428240,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 619024,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "d7cb8422bee1572c22c3f54634da686f9ccdb177",
+          "message": "Move MLD_MUST_CHECK_RETURN_VALUE from randombytes to mlk_randombytes\n\nMLD_MUST_CHECK_RETURN_VALUE was misplaced at randombytes.\nThis commit moves it to mlk_randombytes.\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-01-24T10:44:08+08:00",
+          "tree_id": "e0a1dc2af1125a49e72f8cb118d1dc4d4faded80",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/d7cb8422bee1572c22c3f54634da686f9ccdb177"
+        },
+        "date": 1769222982917,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 214226,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 765477,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 230665,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 381039,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1254106,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 372145,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 604386,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1594850,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 618598,
             "unit": "cycles"
           }
         ]

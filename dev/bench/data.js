1bb479eba54a408abb8ea2b0f938c9d14da74b34
github-action-benchmark <github@users.noreply.github.com>
2026-02-02 09:50:28 +0000

    add Graviton4 (no-opt) (customSmallerIsBetter) benchmark result for c7302f67217d1e24bac630603f8edbf89bc29591

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770025825283,
+  "lastUpdate": 1770025828616,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -351175,6 +351175,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 372785,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "c7302f67217d1e24bac630603f8edbf89bc29591",
+          "message": "Bump actions/checkout from 5.0.0 to 6.0.2\n\nBumps [actions/checkout](https://github.com/actions/checkout) from 5.0.0 to 6.0.2.\n- [Release notes](https://github.com/actions/checkout/releases)\n- [Changelog](https://github.com/actions/checkout/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/actions/checkout/compare/08c6903cd8c0fde910a37f88322edcfb5dd907a8...de0fac2e4500dabe0009e67214ff5f5447ce83dd)\n\n---\nupdated-dependencies:\n- dependency-name: actions/checkout\n  dependency-version: 6.0.2\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n...\n\n- Also update the actions/checkout's version in\n  .github/actions/cbmc/action.yml manually.\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-02-02T17:46:15+08:00",
+          "tree_id": "9a5451883d466e575e28c86fe46662e9b7a3dbef",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/c7302f67217d1e24bac630603f8edbf89bc29591"
+        },
+        "date": 1770025826829,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 128330,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 447585,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 144671,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 220146,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 727285,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 223031,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 365137,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 926039,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 372754,
             "unit": "cycles"
           }
         ]

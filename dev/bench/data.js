d46c759955cf6b29e3e30ba3a5f1fef7c17a6d77
github-action-benchmark <github@users.noreply.github.com>
2026-02-03 14:10:35 +0800

    add SpacemiT K1 8 (Banana Pi F3) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for 3b61742a88c956fa22a24e91e06a217132c4e000

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770098957040,
+  "lastUpdate": 1770099034130,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -90793,6 +90793,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 2410834,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "3b61742a88c956fa22a24e91e06a217132c4e000",
+          "message": "CI: Add AWS-LC integration CI testing against v1.67.0\n\nCore differences to mlkem-native:\n - We only test against a fixed version, not against most recent main (the test\n   against main does not seem to reveal interesting problems so far anyway)\n - We fix a typo integration-awslc.yml matrix.flags -> matrix.test.flags\n\nI have opened https://github.com/pq-code-package/mlkem-native/pull/1535 to\nconsolidate those.\n\n- Resolves https://github.com/pq-code-package/mldsa-native/issues/902\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-02-03T13:57:51+08:00",
+          "tree_id": "7ee25f8406bb1bec31d319f7876d44ce2cfff125",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/3b61742a88c956fa22a24e91e06a217132c4e000"
+        },
+        "date": 1770098706188,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 829815,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 3234030,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 923063,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 1412184,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 5346920,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 1480756,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 2311574,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 6669867,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 2415104,
             "unit": "cycles"
           }
         ]

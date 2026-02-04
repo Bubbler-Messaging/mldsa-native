e566895b552f95fe76b4d9fccdadb25ada41f60f
github-action-benchmark <github@users.noreply.github.com>
2026-02-04 03:59:29 +0000

    add AMD EPYC 4th gen (c7a) (no-opt) (customSmallerIsBetter) benchmark result for 1b60df0de7149bc5890e0e92bbeb0813f8d35f61

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770177561637,
+  "lastUpdate": 1770177568837,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -310883,6 +310883,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 347387,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "1b60df0de7149bc5890e0e92bbeb0813f8d35f61",
+          "message": "ACVP: Correct max message length from 65536 bytes to 65536 bits (8192 bytes)\n\nThe ACVP specification (https://pages.nist.gov/ACVP/draft-celi-acvp-ml-dsa.html)\nsays that the message length is at most 65536 bits (8192 bytes).\nWe incorrectly used 65536 bytes which results in excessive memory allocation\nin acvp_mldsa.c  which can be problematic on devices with limited stack/memory.\nThis commit corrects the maximum length.\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-02-04T11:54:58+08:00",
+          "tree_id": "6e0a50d2ac6708032fac73288b1a98bea99aea1a",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/1b60df0de7149bc5890e0e92bbeb0813f8d35f61"
+        },
+        "date": 1770177567978,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 122116,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 453043,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 131518,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 203971,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 727688,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 209415,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 336689,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 924243,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 346542,
             "unit": "cycles"
           }
         ]

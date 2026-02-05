85343283c921da9d57957b1708efcc34af9b6d76
github-action-benchmark <github@users.noreply.github.com>
2026-02-05 04:55:04 +0000

    add Intel Xeon 4th gen (c7i) (no-opt) (customSmallerIsBetter) benchmark result for 41da557c34d9065055f93dc29f31924f0b090eaa

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770267291298,
+  "lastUpdate": 1770267304110,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -267900,6 +267900,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 270412,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "41da557c34d9065055f93dc29f31924f0b090eaa",
+          "message": "CI: Run apt update before installing other software via apt\n\nWe are seeing errors in CI when trying to install nix via apt.\nThis commit adds an apt update before to fix the issue.\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-02-05T12:51:48+08:00",
+          "tree_id": "17b18963501a81f86176f7850dab1c784a95f09d",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/41da557c34d9065055f93dc29f31924f0b090eaa"
+        },
+        "date": 1770267303658,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 93889,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 333450,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 99851,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 160390,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 545908,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 160887,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 267405,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 707235,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 269967,
             "unit": "cycles"
           }
         ]

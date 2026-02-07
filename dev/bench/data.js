035d80a75d14b45c131ec15fc9dec69742eba656
github-action-benchmark <github@users.noreply.github.com>
2026-02-07 04:03:12 +0000

    add Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt) (customSmallerIsBetter) benchmark result for d06f9ad5156ba0a575a0d963f159d54e00ffcb1c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770436792794,
+  "lastUpdate": 1770436989531,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -22420,6 +22420,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 629068,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "distinct": true,
+          "id": "d06f9ad5156ba0a575a0d963f159d54e00ffcb1c",
+          "message": "x86_64/AArch64: Add .size directive to all assembly functions\n\nAdd size information to function symbols.\nsize information is added through a MLD_ASM_FN_SIZE macro (mapping to .size\nfor elf-targets and nothing otherwise) which gets added automatically by\nautogen. The makes the assembly\nfunctions show up with their correct size in the elf instead of always\nhaving a zero size, making it easier to see how much space each\nfunction takes up.\n\nCo-authored-by: Anders Sonmark <Anders.Sonmark@axis.com>\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-02-07T03:49:20Z",
+          "tree_id": "517aee7a579c64c618c74f25223d9f881189a028",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/d06f9ad5156ba0a575a0d963f159d54e00ffcb1c"
+        },
+        "date": 1770436985003,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 236052,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 629413,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 227072,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 395068,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1052618,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 389099,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 670527,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1377415,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 652907,
             "unit": "cycles"
           }
         ]

6dca11d7283c0353bc45da5fa99d4c5df40e082b
github-action-benchmark <github@users.noreply.github.com>
2026-02-08 08:18:47 +0000

    add AMD EPYC 3rd gen (c6a) (no-opt) (customSmallerIsBetter) benchmark result for 26ce481ae714c5a01ae96e5fa4e6eafe1aa83d94

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770538715639,
+  "lastUpdate": 1770538727492,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -296050,6 +296050,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 383035,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "Anders.Sonmark@axis.com",
+            "name": "Anders Sonmark",
+            "username": "flynd"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "26ce481ae714c5a01ae96e5fa4e6eafe1aa83d94",
+          "message": "native: Give single assembly unit a unique name\n\nGive the single compilation units unique names by renaming the assembly\nfile.  This removes the need for having unique names for object files\nbased on the source language and allows simpler rules, for example\n\"%.o: %.c\" and \"%.o: %.S\".\n\nSigned-off-by: Anders Sonmark <Anders.Sonmark@axis.com>",
+          "timestamp": "2026-02-08T16:14:42+08:00",
+          "tree_id": "6b62d5ed57573484e8e3c427d58711671015c188",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/26ce481ae714c5a01ae96e5fa4e6eafe1aa83d94"
+        },
+        "date": 1770538726765,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 134562,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 524531,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 147768,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 226999,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 860204,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 234970,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 370479,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1078643,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 382584,
             "unit": "cycles"
           }
         ]

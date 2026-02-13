7315937a5feb81be2d391a8f1267d26b368e3a56
github-action-benchmark <github@users.noreply.github.com>
2026-02-13 11:50:55 +0000

    add Graviton4 (no-opt) (customSmallerIsBetter) benchmark result for fdfe6ae3342a17973191f17ed025913b6aca5a44

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770983452291,
+  "lastUpdate": 1770983455501,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -371116,6 +371116,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 372421,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "distinct": true,
+          "id": "fdfe6ae3342a17973191f17ed025913b6aca5a44",
+          "message": "Autogen: Auto-generate polyz_unpack TBL index tables from autogen\n\nMove the hand-written polyz_unpack_{17,19}_indices tables to be\nauto-generated from a Python generator in scripts/autogen.\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-02-13T11:46:17Z",
+          "tree_id": "34be7f6346bd41a113d3f666c902d77e566de806",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/fdfe6ae3342a17973191f17ed025913b6aca5a44"
+        },
+        "date": 1770983454533,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 128128,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 447321,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 142189,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 220631,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 726434,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 223098,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 365046,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 926646,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 372427,
             "unit": "cycles"
           }
         ]

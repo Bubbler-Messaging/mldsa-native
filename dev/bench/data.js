b1b759f81c26368dbcaffa70abe244670dc3c10c
github-action-benchmark <github@users.noreply.github.com>
2026-02-13 04:37:41 +0000

    add Graviton3 (customSmallerIsBetter) benchmark result for d3eb91bde875a8c7d382253f723eff818e9d2f0d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770957445079,
+  "lastUpdate": 1770957460888,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -391883,6 +391883,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 205715,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "distinct": true,
+          "id": "d3eb91bde875a8c7d382253f723eff818e9d2f0d",
+          "message": "Lint: Ignore unavailable cr.yp.to and mceliece.org links\n\nDJB's domains are often unreachable causing our markdown link lint jobs to\nfail. This commit changes the CI job to skip those domains.\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-02-13T04:32:47Z",
+          "tree_id": "7d14c9e43582c8ee626e85853801c0372d2e04dd",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/d3eb91bde875a8c7d382253f723eff818e9d2f0d"
+        },
+        "date": 1770957362219,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 72300,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 211864,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 75649,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 127542,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 350245,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 125450,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 207990,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 449004,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 205654,
             "unit": "cycles"
           }
         ]

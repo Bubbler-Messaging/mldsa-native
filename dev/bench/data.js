92bcaade6495f53b7d55b7f35f8955d40413de66
github-action-benchmark <github@users.noreply.github.com>
2026-02-14 05:11:55 +0000

    add Graviton3 (no-opt) (customSmallerIsBetter) benchmark result for 39fe99557325be19c2b1ce2ac6535f81deec00ba

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771045885821,
+  "lastUpdate": 1771045914827,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -418377,6 +418377,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 402280,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "distinct": true,
+          "id": "39fe99557325be19c2b1ce2ac6535f81deec00ba",
+          "message": "Armv8.1-M: Disable backend by default\n\nThe Armv8.1-M + MVE backend is still in active development and has\nnot undergone the same level of audit as the rest of the code.\n\nThis commit extends the documentation to make this clear.\n\nThe commit also disables the Armv8.1-M + MVE backend by default,\nand instead explicitly enables it in the an547 baremetal Makefile.\n\n- Port of https://github.com/pq-code-package/mlkem-native/commit/9d2f1c24f30b9747cc8d45d67052ddb0a4b597f0\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-02-14T05:06:40Z",
+          "tree_id": "ffac40a21a71237304205f52fd4543194ed94416",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/39fe99557325be19c2b1ce2ac6535f81deec00ba"
+        },
+        "date": 1771045914418,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 138474,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 484034,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 156390,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 241090,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 792018,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 241077,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 396383,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1012659,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 402288,
             "unit": "cycles"
           }
         ]

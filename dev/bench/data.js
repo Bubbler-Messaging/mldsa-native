4beb6fb345a8160c99f5d66ce94b76600c859c25
github-action-benchmark <github@users.noreply.github.com>
2026-03-03 14:41:14 +0000

    add AMD EPYC 3rd gen (c6a) (no-opt) (customSmallerIsBetter) benchmark result for 4398820258860fa7894dc1d45a0a626e5dd93f18

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772548869935,
+  "lastUpdate": 1772548873822,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -307780,6 +307780,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 384219,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "distinct": true,
+          "id": "4398820258860fa7894dc1d45a0a626e5dd93f18",
+          "message": "CI: Disable benchmarking CI on self hosted boards\n\nDue to relocation, we have to temporarily disable CI that is running on most\nof our self-hosted runners:\n - Arm Cortex-A72 (Raspberry Pi 4)\n - Arm Cortex-A55 (Snapdragon 888)\n - SpacemiT K1 8 (Banana Pi F3)\n -  Mac Mini (M1, 2020)\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-03-03T14:36:51Z",
+          "tree_id": "e7660c3ef8a2bc7b491a8913677f27de38f31f6c",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/4398820258860fa7894dc1d45a0a626e5dd93f18"
+        },
+        "date": 1772548873053,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 134614,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 524403,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 147443,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 227114,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 860722,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 234886,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 372687,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1083594,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 385166,
             "unit": "cycles"
           }
         ]

bc3c3209569d77ec36c3611918f82fe308413035
github-action-benchmark <github@users.noreply.github.com>
2026-03-03 10:49:08 +0000

    add Graviton2 (no-opt) (customSmallerIsBetter) benchmark result for 8b11739463aa9e55d4974fba5b55db2679e1ba36

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772534912634,
+  "lastUpdate": 1772534947647,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -469571,6 +469571,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 618643,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rodchap@amazon.com",
+            "name": "Rod Chapman",
+            "username": "rod-chapman"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "8b11739463aa9e55d4974fba5b55db2679e1ba36",
+          "message": "Correct operator precedence in ensures clause of mld_caddq()\n\nOperator == binds more tightly than ternary ? : so\nparentheses are needed here to ensure correctness\nand readability.\n\nSigned-off-by: Rod Chapman <rodchap@amazon.com>",
+          "timestamp": "2026-03-03T18:43:04+08:00",
+          "tree_id": "54905a6c39483e62e088e2d147d97676d9a5d3ce",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/8b11739463aa9e55d4974fba5b55db2679e1ba36"
+        },
+        "date": 1772534947146,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 212475,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 760050,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 234192,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 380249,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1253411,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 371718,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 604125,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1594280,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 618317,
             "unit": "cycles"
           }
         ]

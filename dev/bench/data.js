09d9559f55fb1c4bf7daa139562c05d75d049633
github-action-benchmark <github@users.noreply.github.com>
2026-03-03 13:28:09 +0000

    add Graviton2 (no-opt) (customSmallerIsBetter) benchmark result for 08e63cc0d4b1491a09f608f4876758abd3f86fa7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772544453184,
+  "lastUpdate": 1772544488748,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -470537,6 +470537,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 618317,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rodchap@amazon.com",
+            "name": "Rod Chapman",
+            "username": "rod-chapman"
+          },
+          "committer": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "distinct": true,
+          "id": "08e63cc0d4b1491a09f608f4876758abd3f86fa7",
+          "message": "Correct assertion in polyveck_add()\n\nIt was previously asserting the correct range on values in\nu->vec in the range 0 .. <MLDSA_L, but this should be\n0 .. <MLDSA_K.\n\nSince MLDSA_L <= MLDSA_K for all parameter sets, this passes\nproof and runtime checking, but is weak.\n\nThe loop-invariant and post-condition of this function\nare correct, so proof of calling units is not affected.\n\nSigned-off-by: Rod Chapman <rodchap@amazon.com>",
+          "timestamp": "2026-03-03T13:22:06Z",
+          "tree_id": "023e1aef6fa62e21274df66c0a231ec2e045f24f",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/08e63cc0d4b1491a09f608f4876758abd3f86fa7"
+        },
+        "date": 1772544488257,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 212544,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 760305,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 234271,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 380269,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1253516,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 371799,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 604901,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1596012,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 619143,
             "unit": "cycles"
           }
         ]

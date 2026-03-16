724fe034dd21685da791ff9fd579b0c824403053
Dusan Kostic <dkostic@amazon.com>
2026-03-16 16:26:01 +0000

    aarch64 rejection sampling proof
    Signed-off-by: Dusan Kostic <dkostic@amazon.com>

--- proofs/hol_light/aarch64/proofs/dump_bytecode.ml
+++ proofs/hol_light/aarch64/proofs/dump_bytecode.ml
@@ -13,3 +13,7 @@ print_string "==== bytecode end =====================================\n\n";;
 print_string "=== bytecode start: aarch64/mldsa/mldsa_poly_caddq.o ===\n";;
 print_literal_from_elf "aarch64/mldsa/mldsa_poly_caddq.o";;
 print_string "==== bytecode end =====================================\n\n";;
+
+print_string "=== bytecode start: aarch64/mldsa/mldsa_rej_uniform.o ===\n";;
+print_literal_from_elf "aarch64/mldsa/mldsa_rej_uniform.o";;
+print_string "==== bytecode end =====================================\n\n";;

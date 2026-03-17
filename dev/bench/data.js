742d10cfd5781f5be492c7d078b46f5ba221fe94
github-action-benchmark <github@users.noreply.github.com>
2026-03-17 06:41:33 +0000

    add Graviton4 (customSmallerIsBetter) benchmark result for 7ae414cc7756ac009ee613f81aaa4090fcf1fbfb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773729683718,
+  "lastUpdate": 1773729693290,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -356973,6 +356973,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 194364,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "7ae414cc7756ac009ee613f81aaa4090fcf1fbfb",
+          "message": "check-contracts: Limit number of loops without decreases clause\n\nPreviously, check-contracts would only allow listing entire functions\nas exceptions to the loop termination checker, thereby allowing _all_\nloops in a function to remain without termination clauses.\n\nThis commit allows to specify how many loops in a function may be\nleft without a termination clause. This would have caught the case\nof poly_challenge missing termination clauses for two of its three\nloops, which the previous commit fixed.\n\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>",
+          "timestamp": "2026-03-17T14:37:25+08:00",
+          "tree_id": "f67f8d57307164a1224f5a09aaff41c3b80d1a4b",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/7ae414cc7756ac009ee613f81aaa4090fcf1fbfb"
+        },
+        "date": 1773729601416,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 68055,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 201923,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 70656,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 121049,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 331245,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 117915,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 198447,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 427430,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 194509,
             "unit": "cycles"
           }
         ]

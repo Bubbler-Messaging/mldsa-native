aa565bb405fe5c50a9e8aa03d2f1f2912a89b8a4
github-action-benchmark <github@users.noreply.github.com>
2026-04-05 05:15:02 +0000

    add Graviton3 (customSmallerIsBetter) benchmark result for b20a5b83b9a7a05a9b2e101a408d997d9eab6438

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775366098699,
+  "lastUpdate": 1775366102498,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -411134,6 +411134,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 205873,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@zerorisc.com",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "distinct": true,
+          "id": "b20a5b83b9a7a05a9b2e101a408d997d9eab6438",
+          "message": "MAINTAINERS.md: Remove affiliations\n\nI recently switched from Chelpis to zeroRISC meaning our MAINTAINERS.md\nis outdated.\nThis commit removes affilations from the file as they are unnecessary.\n\nSigned-off-by: Matthias J. Kannwischer <matthias@zerorisc.com>",
+          "timestamp": "2026-04-05T06:10:03+01:00",
+          "tree_id": "39010b48748aa070b72d83bc81c81a848e1ebdb7",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/b20a5b83b9a7a05a9b2e101a408d997d9eab6438"
+        },
+        "date": 1775365983017,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 72234,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 213475,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 75738,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 127602,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 353349,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 125722,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 208467,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 452572,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 205885,
             "unit": "cycles"
           }
         ]

ac984e5f5045a67c0e787fd2f825b2f9f70e0e45
github-action-benchmark <github@users.noreply.github.com>
2026-04-06 04:58:06 +0000

    add Graviton4 (no-opt) (customSmallerIsBetter) benchmark result for 094b90794aaaa9fa9d74b4c3b294464cc9261cca

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775451479692,
+  "lastUpdate": 1775451485861,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -389263,6 +389263,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 373877,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "willie.zhao@chelpis.com",
+            "name": "willieyz",
+            "username": "willieyz"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "094b90794aaaa9fa9d74b4c3b294464cc9261cca",
+          "message": "CBMC/AArch64: Port native Keccak F1600 CBMC proofs\n\nThis commit adds the CBMC proofs for the native AArch64 Keccak routines from\nmlkem-native. This shows that the native contracts fulfills our API contracts.\n\nSigned-off-by: willieyz <willie.zhao@chelpis.com>",
+          "timestamp": "2026-04-06T12:53:10+08:00",
+          "tree_id": "15e6f624751a221f4313eaabb6463b11cf234ff6",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/094b90794aaaa9fa9d74b4c3b294464cc9261cca"
+        },
+        "date": 1775451481082,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 128328,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 448137,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 144836,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 220823,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 730005,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 223825,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 367176,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 929610,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 374400,
             "unit": "cycles"
           }
         ]

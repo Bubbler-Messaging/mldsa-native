5829f0aedef780c5ab36a9c98eb2ab44e7bf18ed
github-action-benchmark <github@users.noreply.github.com>
2026-04-12 07:14:19 +0000

    add Intel Xeon 3rd gen (c6i) (customSmallerIsBetter) benchmark result for af478997a133f4be1502a568b04ea80fbb6d8ed1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775978048232,
+  "lastUpdate": 1775978058706,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -171586,6 +171586,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 162040,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "af478997a133f4be1502a568b04ea80fbb6d8ed1",
+          "message": "CT: Update comment on valgrind workaround\n\nWe currently pass `--vex-guest-max-insns=55` to the valgrind\ntests to work around an issue in the x86 backend.\n\nThe issue persists in v3.26; this comment updates the comment accordingly.\n\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>",
+          "timestamp": "2026-04-12T15:09:49+08:00",
+          "tree_id": "c1125a5bb0c845263561741331ce19d01261949e",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/af478997a133f4be1502a568b04ea80fbb6d8ed1"
+        },
+        "date": 1775977932364,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 56800,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 181536,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 61446,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 100656,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 299685,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 102242,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 166751,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 369929,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 161796,
             "unit": "cycles"
           }
         ]

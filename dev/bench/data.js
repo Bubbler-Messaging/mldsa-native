26f727b483516eb047da1ed8a1ab40c68f7f5177
github-action-benchmark <github@users.noreply.github.com>
2026-04-14 06:29:37 +0000

    add Intel Xeon 3rd gen (c6i) (no-opt) (customSmallerIsBetter) benchmark result for 4d393aa9e49112a9269f8b58dbb4ddf622103f3b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776148171244,
+  "lastUpdate": 1776148177452,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -246928,6 +246928,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 458861,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@zerorisc.com",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "distinct": true,
+          "id": "4d393aa9e49112a9269f8b58dbb4ddf622103f3b",
+          "message": "CI: Re-enable benchmarking on Mac Mini/RPi4/A55/BPi\n\nPort of pq-code-package/mlkem-native#1641.\n\nSigned-off-by: Matthias J. Kannwischer <matthias@zerorisc.com>",
+          "timestamp": "2026-04-14T07:24:38+01:00",
+          "tree_id": "8fc4bb063ce094b0acb7a1447ce7fb94be81172b",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/4d393aa9e49112a9269f8b58dbb4ddf622103f3b"
+        },
+        "date": 1776148176338,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 157195,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 546188,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 168923,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 268289,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 893826,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 274805,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 448073,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1155454,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 457615,
             "unit": "cycles"
           }
         ]

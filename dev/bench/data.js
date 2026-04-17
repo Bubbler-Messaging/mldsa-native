63faf08eb258c30fe85869ae81208773f8777502
github-action-benchmark <github@users.noreply.github.com>
2026-04-17 07:57:24 +0000

    add AMD EPYC 3rd gen (c6a) (customSmallerIsBetter) benchmark result for aced043c76e6a6e299f8bdfc40c9dd431412cc9c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776412629475,
+  "lastUpdate": 1776412643927,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -199736,6 +199736,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 195265,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "aced043c76e6a6e299f8bdfc40c9dd431412cc9c",
+          "message": "CT: Clarify that t0 is public despite it not being part of the pk\n\nThis commit adds a reference to FIPS204 that states that t0 does not need to\nbe considered secret. That replaces an old reference to an eprint report\nstating the same.\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-04-17T09:53:09+02:00",
+          "tree_id": "626df39195e6d6f163408c9adf7b3eff831dfbc4",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/aced043c76e6a6e299f8bdfc40c9dd431412cc9c"
+        },
+        "date": 1776412539657,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 69040,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 189166,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 69356,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 119289,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 302773,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 115824,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 204759,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 398302,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 196410,
             "unit": "cycles"
           }
         ]

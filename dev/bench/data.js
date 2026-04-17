2148bcff0a11bc1663feef16f82b1f7735f8086a
github-action-benchmark <github@users.noreply.github.com>
2026-04-17 09:49:10 +0000

    add Arm Cortex-A55 (Snapdragon 888) benchmarks (opt) (customSmallerIsBetter) benchmark result for 398f301eed7e0521edae7477cf94be467bf363be

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776419017236,
+  "lastUpdate": 1776419348574,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -529254,6 +529254,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 786709,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "distinct": true,
+          "id": "398f301eed7e0521edae7477cf94be467bf363be",
+          "message": "CBMC: Remove MLD_UNION_OR_STRUCT\n\nPreviously, lack of support for `union`s in CBMC led us to\nintroduce a macro MLD_UNION_OR_STRUCT dispatching between\nstructs and unions. For CBMC proofs, we would use structs,\nbut for RAM-optimized builds, we would use unions.\n\nNow that CBMC v6.9.0 has been released with much improved\nsupport for unions, we can unconditionally use `union`s.\n\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>",
+          "timestamp": "2026-04-17T10:36:54+01:00",
+          "tree_id": "4261da1a0c7d5aadf2dc690561d5c2b9fa2ff1e7",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/398f301eed7e0521edae7477cf94be467bf363be"
+        },
+        "date": 1776418813594,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 274641,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 811138,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 273976,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 469688,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1320634,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 452540,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 811469,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1820982,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 781856,
             "unit": "cycles"
           }
         ]

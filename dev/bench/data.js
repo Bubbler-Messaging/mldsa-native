96589869a3fd8ca755cd5c3fa242fb3ff5038b41
github-action-benchmark <github@users.noreply.github.com>
2026-04-17 07:57:40 +0000

    add Graviton4 (customSmallerIsBetter) benchmark result for aced043c76e6a6e299f8bdfc40c9dd431412cc9c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776412653902,
+  "lastUpdate": 1776412659915,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -376707,6 +376707,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 194794,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "aced043c76e6a6e299f8bdfc40c9dd431412cc9c",
+          "message": "CT: Clarify that t0 is public despite it not being part of the pk\n\nThis commit adds a reference to FIPS204 that states that t0 does not need to\nbe considered secret. That replaces an old reference to an eprint report\nstating the same.\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-04-17T09:53:09+02:00",
+          "tree_id": "626df39195e6d6f163408c9adf7b3eff831dfbc4",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/aced043c76e6a6e299f8bdfc40c9dd431412cc9c"
+        },
+        "date": 1776412545915,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 68142,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 203501,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 70805,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 120797,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 332024,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 118021,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 198307,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 429323,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 194678,
             "unit": "cycles"
           }
         ]
